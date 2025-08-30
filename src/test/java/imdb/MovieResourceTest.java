ca58020c9c61cc4f7d713f5d7022d69b356a6fbc
Anton Hrytsenko <hrytsenko.anton@gmail.com>
2025-08-30 19:36:32 +0300

    Replace library for JSON comparison.

--- src/test/java/imdb/MovieResourceTest.java
+++ src/test/java/imdb/MovieResourceTest.java
@@ -1,13 +1,12 @@
 package imdb;
 
 import static io.restassured.RestAssured.given;
-import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONCompareMode;
 
 @QuarkusTest
 @TestProfile(Profiles.CustomDataset.class)
@@ -21,35 +20,33 @@ class MovieResourceTest {
         .when().get("/movies")
         .then().statusCode(200)
         .extract().asString();
-
-    String expectedMovies = """
-        {
-          "total": 1,
-          "movies": [
+    assertThatJson(actualMovies)
+        .isEqualTo("""
             {
-              "rank": 153,
-              "title": "The Thing",
-              "year": 1982,
-              "rating": "8.2",
-              "genres": [
-                "Horror",
-                "Mystery",
-                "Sci-fi"
-              ],
-              "casts": [
-                "Kurt Russell",
-                "Wilford Brimley"
-              ],
-              "writers": [
-                "Bill Lancaster",
-                "John W. Campbell Jr."
+              "total": 1,
+              "movies": [
+                {
+                  "rank": 153,
+                  "title": "The Thing",
+                  "year": 1982,
+                  "rating": "8.2",
+                  "genres": [
+                    "Horror",
+                    "Mystery",
+                    "Sci-fi"
+                  ],
+                  "casts": [
+                    "Kurt Russell",
+                    "Wilford Brimley"
+                  ],
+                  "writers": [
+                    "Bill Lancaster",
+                    "John W. Campbell Jr."
+                  ]
+                }
               ]
             }
-          ]
-        }
-        """;
-
-    assertEquals(expectedMovies, actualMovies, JSONCompareMode.STRICT);
+            """);
   }
 
 }
